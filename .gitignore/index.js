8e9f12ef9025ba3482960c1e41b382bc5f54e9db
VirToXexe <48843742+VirToXexe@users.noreply.github.com>
2019-08-15 17:52:04 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -169,6 +169,24 @@ client.on('message', message => {
 }
 )
 
+client.on('message', message => {
+  if (message.content === `${prefix}credit`){
+
+    let serverembed = new Discord.RichEmbed()
+    .setTitle("**Credits:** ⭐")
+    .setThumbnail(`${message.author.avatarURL}`)
+    .setColor("FE656F")
+    .addField("Créateur du bot:","VirToX.")
+    .addField("Ses résaux:",'Discord: "＂۷เгŦ๏χ#4969 \nAutres résaux: perso :x:')
+    .addField("Invinter le bot", "https://discordapp.com/oauth2/authorize?client_id=606482261780594711&scope=bot&permissions=2146958591 **vous pouvez ajustez les permissions du BOT $$**")
+    .setFooter("By ＂۷เгŦ๏χ#4969", message.author.avatarURL)
+    .setTimestamp()
+
+    return message.channel.send(serverembed);
+  }
+}
+)
+
 /*Kick*/
 client.on('message',message =>{
     if (!message.guild) return
